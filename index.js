639a38fc04f9026183cbb8e7b21db35e98fc06da
rohitprasad1203 <iamrohit1203@gmail.com>
2021-02-06 17:24:43 +0530

    complete

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+// code for alarm 
 
 let submit = document.getElementById('submit');
 
-submit.addEventListener('click',alarm);
+submit.addEventListener('click',alarm);  // adding event listner
 
 audio = new Audio('salamisound-1532427-countdown-e-g-for-bomb-or.mp3');
 
-function play(){
+function play(){                 // audio for alarm
     audio.play();
 }
 
 function alarm(e){
-    e.preventDefault();
+    e.preventDefault(); // stop reloading on submit
 
     let time = document.getElementById('time');
 
-    date = new Date(time.value);
+    date = new Date(time.value); // input date time for alarm
 
-    now = new Date();
+    now = new Date(); // current timing Date
 
-    alrmTime = date - now ;
+    alrmTime = date - now ; // how much millisecond left for alarm
 
     console.log(alrmTime);
 
@@ -29,4 +30,21 @@ function alarm(e){
     }
     
 }
+
+
+// code for current time 
+
+setInterval(() => {
+    myDate = new Date();
+
+    options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var h=myDate.getHours();  
+    var m=myDate.getMinutes();  
+    var s=myDate.getSeconds();
+    if (h<10){h = 0+''+h}
+    if (m<10){m = 0+''+m}
+    if (s<10){s = 0+''+s}
+    document.getElementById('curtime').innerHTML = h+":"+m+":"+s;
+    document.getElementById('date').innerHTML = myDate.toLocaleDateString(undefined,options);
+}, 1000);
 
